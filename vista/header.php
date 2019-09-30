4d1c3d70bb9468fd26f137dddaaa82d6f875931c
Eduardo Palomera <lalox1365@gmail.com>
2019-09-30 09:37:31 -0500

    Archivos publicos y Header
    Se agregaron las carpetas para los archivos css y javascript ademas de la implementacion del header para las vistas

--- vista/header.php
+++ vista/header.php
@@ -1,49 +1,45 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	<title>Vinateria</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="col-sm-4">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">Vinatería</a>
+				</div>
+			</div>
+			<div class="col-sm-4">
+			<ul class="nav navbar-nav">
+				<li>
+					<form>
+						<div class="form-group row">
+							<div class="col-xs-12">
+								<input class="form-control" id="ex1" type="text" placeholder="Buscar">
+							</div>
+						</div>
+					</form>
+				</li>
+			</ul>
+		</div>
+		<div class="col-sm-4">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="nav navbar-nav navbar-right"><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			</ul>
+		</div>
+		</div>
+	</nav>
 
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Vinatería</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">
-      	<div class="container">
-      	<form>
-    	<div class="form-group row">
-      	<div class="col-xs-2">
-	        
-    	    <input class="form-control" id="ex1" type="text" placeholder="Buscar">
-      	</div>
-      
-    	</div>
-  		</form>
-  		</div>
-      </a></li>
-    </ul>
-
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
-
-  </div>
-</nav>
-  
-<div class="container">
-</div>
+	<div class="container">
+	</div>
 
 </body>
 </html>
