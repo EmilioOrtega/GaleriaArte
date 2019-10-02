19f67d2136990f24f671946766a032482c7f66b5
skillerhero <skillerhero@users.noreply.github.com>
2019-10-01 23:22:20 -0500

    Login, registrar usuario, verificar tarjeta
    Se hizo la clase ModeloUsuario que contiene los métodos de registrar usuario y login. También se creó la clase ModeloTarjeta que tiene el método verificarTarjeta. Y, se hicieron los archivos registrarUsuario.php, login.php, verificarTarjeta.php para probar estos métodos.

--- modelo/registrarUsuario.php
+++ modelo/registrarUsuario.php
@@ -1,5 +1,5 @@
  <?php
 	include 'ModeloUsuario.php';
-	$usuario = new ModeloUsuario();
-	$usuario->registroUsuario("skillerhero","contra","","rafael","leon","m","3323403259","1999-12-16",'a',1);
+	$usuario = new Usuario();
+	$usuario->registrarUsuario("usuario","contra","nombre","apellido","sexo","123456789123","1990-10-10",'a',1);
 ?> 
