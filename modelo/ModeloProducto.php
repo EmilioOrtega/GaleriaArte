7966701c0074ab9dd2675ca37097820803913f5f
Gabriel Rodriguez <48400046+bonnesbaby14@users.noreply.github.com>
2019-10-02 00:10:21 -0500

    Se corrigio modelo de Producto

--- modelo/ModeloProducto.php
+++ modelo/ModeloProducto.php
@@ -8,39 +8,10 @@ include '../libs/Modelo.php';
 		parent::__construct();
 	}
 
-	public function mostrarProductos()
+
+	public function getProducto($id)
 	{
-		$result=$this->getProductos();
-
-		if ($result->num_rows > 0) 
-		{
-			echo "Productos:"."<br>";
-
-		    // output data of each row
-		    while($row = $result->fetch_assoc()) 
-		    {
-		        echo $row["id"]. "<br>";
-		        echo $row["producto"]. "<br>";
-		        echo $row["contenido"]. "<br>";
-		        echo $row["categoria"]. "<br>";
-		        echo $row["precio"]. "<br>";
-		        echo $row["descripcion"]. "<br>";
-		        echo $row["cantidad"]. "<br>";
-		        echo $row["imagen"]. "<br>";
-		        echo $row["descuento"]. "<br>";
-		        echo $row["marca"]. "<br>";
-		        echo $row["origen"]. "<br>";
-		    }
-		} 
-		else 
-		{
-		    echo "No hay productos registrados";
-		}
-	}
-
-	public function getProductos()
-	{
-		$sql = "SELECT * FROM productos";
+		$sqsl = "SELECT * FROM productos where id=".$id."";
 		$result = $this->conexion->query($sql);
 		$this->conexion->close();
 		return $result;
