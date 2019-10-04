7f08c671121d2b530df89cdec8faef022e1bc342
Gabriel Rodriguez <48400046+bonnesbaby14@users.noreply.github.com>
2019-10-03 23:24:58 -0500

    controlador users  y modelo

--- controlador/ControladorUsers.php
+++ controlador/ControladorUsers.php
@@ -8,7 +8,7 @@ class Users extends Controlador{
 	}
 
 	function index() {
-		$producto = $this->modelo->getProducto($_POST['id']);
+		$users = $this->modelo->getUsers();
 		echo '
 		<br>
 		<br>
@@ -17,40 +17,20 @@ class Users extends Controlador{
 
 
             <tr>
-                <td>User</td>
+                <td>Usuario</td>
                 <td>Nombres</td>
-                <td>Apellidos P</td>
-                <td>Apellidos M</td>
-                <td>Celular</td>
-                <td>Domicilio</td>
-                <td>Municipio</td>
-                <td>Colonia</td>
-                <td>Registro</td>
-                <td>Carrera</td>
+                <td>Apellidos</td>
+                <td>Sexo</td>
+                <td>Telefono</td>
+                <td>Fecha nacimiento</td>
+                <td>Tipo de usuario</td>
+                <td>Tarjeta</td>
                 <td>Editar</td>
                 <td>Eliminar</td>
             </tr>
 
-                <tr>
-                    <td> <?php echo $ver[0]  ?> </td>
-                    <td><?php echo $ver[1]  ?></td>
-                    <td><?php echo $ver[2]  ?></td>
-                    <td><?php echo $ver[3]  ?></td>
-                    <td><?php echo $ver[4]  ?></td>
-                    <td><?php echo $ver[5]  ?></td>
-                    <td><?php echo $ver[6]  ?></td>
-                    <td><?php echo $ver[7]  ?></td>
-                    <td><?php echo $ver[8]  ?></td>
-                    <td><?php echo $ver[9]  ?></td>
+                
 
-
-
-
-                    <td><button class="btn btn-warning" data-toggle="modal" data-target="#modelEditarA" >Editar</button></td>
-                    <td><button class="btn btn-danger" data-toggle="modal" data-target="#modelEliminarA" >Eliminar</button></td>
-                </tr>
-
-           
 
             
 
@@ -59,7 +39,27 @@ class Users extends Controlador{
 
         
 
-        </table>';
+       ';
+	for ($i=0; $i < count($users); $i++) { 
+			echo '<tr>
+                    <td> <?php echo $ver[0]  ?> </td>
+                    <td><?php echo $ver[1]  ?></td>
+                    <td><?php echo $ver[2]  ?></td>
+                    <td><?php echo $ver[3]  ?></td>
+                    <td><?php echo $ver[6]  ?></td>
+                    <td><?php echo $ver[7]  ?></td>
+                    <td><?php echo $ver[8]  ?></td>
+                    <td><?php echo $ver[9]  ?></td>
+                    <td><button class="btn btn-warning" data-toggle="modal" data-target="#modelEditarA" >Editar</button></td>
+                    <td><button class="btn btn-danger" data-toggle="modal" data-target="#modelEliminarA" >Eliminar</button></td>
+                </tr>
+                
+                ';
+        }
+        echo ' </table>';
+
+
+        
 	}
 }
 ?>
